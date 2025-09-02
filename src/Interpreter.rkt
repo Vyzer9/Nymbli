e983ff5299104c02a8d5aa4db89615c7230a1467
Vyzer9 <Samzoovsk19@gmail.com>
2025-09-01 22:03:40 -0300

    up

--- src/Interpreter.rkt
+++ src/Interpreter.rkt
@@ -1,104 +1,55 @@
 #lang racket
 
-(require "syntax/LogicOperations.rkt"
-         "syntax/Conditional.rkt"
-         "syntax/Lambda.rkt")
 
-;; Ambiente global
-(define global-env (make-hash))
+;; Structs existentes
+(struct func (name params body) #:transparent)
+(struct call (name args) #:transparent)
 
-;; Definir constante
-(define (define-const! env name value)
-  (hash-set! env name value))
+;; Ambiente global (hash mutável)
+(define env (make-hash))
 
-;; Procurar valor de variável
-(define (lookup env name)
-  (hash-ref env name (lambda () (error "Variável não encontrada:" name))))
-
-;; Interpretador principal
+;; Mini interpretador
 (define (interpret expr env)
   (cond
     ;; Literais
     [(number? expr) expr]
-    [(string? expr) expr]
-
+    
     ;; Variáveis
-    [(symbol? expr) (lookup env expr)]
-
-    ;; Operações lógicas/matemáticas simples
+    [(symbol? expr)
+     (hash-ref env expr (lambda () (error "Variável não encontrada:" expr)))]
+    
+    ;; Operações matemáticas
     [(and (list? expr) (symbol? (first expr))
-          (member (first expr) '(+ - * / = > < <= >=)))
-     (eval-op expr interpret env)]
-
-    ;; If (delegado ao módulo Conditional.rkt)
-    [(and (list? expr) (eq? (first expr) 'if))
-     (eval-if expr interpret env)]
-
-    ;; nymconst (definição de variável)
-    [(and (list? expr) (eq? (first expr) 'nymconst))
-     (define name (second expr))
-     (define val (interpret (third expr) env))
-     (define-const! env name val)
-     val]
-
-    ;; PrintNym
-    [(and (list? expr) (eq? (first expr) 'PrintNym))
-     (define val (interpret (second expr) env))
-     (displayln val)
-     val]
-
-    ;; nymlambda (função anônima)
-[(and (list? expr) (eq? (first expr) 'nymlambda))
- (eval-lambda expr env interpret)] ; ✅ novo
-
-
+          (member (first expr) '(+ - * /)))
+     (apply (eval (first expr))
+            (map (lambda (e) (interpret e env)) (rest expr)))]
+    
     ;; Chamada de função
-    [(and (list? expr) (not (member (first expr) '(nymconst PrintNym if + - * / = > < <= >= nymlambda))))
-     (let ([func (interpret (first expr) env)]
-           [args (map (lambda (e) (interpret e env)) (rest expr))])
-       (apply func args))]
+    [(call? expr)
+     (define f (hash-ref env (call-name expr)
+                         (lambda () (error "Função não encontrada:" (call-name expr)))))
+     (define arg-vals (map (lambda (e) (interpret e env)) (call-args expr)))
+     (define new-env
+       (for/fold ([acc env])
+                 ([param (func-params f)]
+                  [arg arg-vals])
+         (hash-set acc param arg)))
+     (interpret (func-body f) new-env)]
 
-    ;; Expressão inválida
     [else (error "Expressão inválida:" expr)]))
 
-;; =====================
-;; Exemplo de Lambda.rkt
-;; =====================
+;; =========================
+;; Criando uma função soma
+;; =========================
+(define soma-func
+  (func 'soma '(a b)
+    '(+ a b)))
 
-(provide eval-lambda)
+;; Adiciona função ao ambiente
+(hash-set! env 'soma soma-func)
 
-(define (eval-lambda expr env)
-  ;; Espera: (nymlambda (params...) body...)
-  (let* ([params (second expr)]
-         [body (cddr expr)])
-    (lambda args
-      (define new-env (for/fold ([acc-env env])
-                                 ([param params]
-                                  [arg args])
-                        (hash-set acc-env param arg)))
-      (define results (map (lambda (e) (interpret e new-env)) body))
-      (if (null? (cdr results))
-          (car results)
-          (last results)))))
+;; Teste de chamada: (soma 3 4)
+(define resultado
+  (interpret (call 'soma '(3 4)) env))
 
-;; =====================
-;; Exemplos de uso
-;; =====================
-
-;; Definindo variável
-(interpret '(nymconst x 10) global-env)
-
-;; Printando x (10)
-(interpret '(PrintNym x) global-env)
-
-;; Printando soma (5)
-(interpret '(PrintNym (+ 2 3)) global-env)
-
-;; If simples (100)
-(interpret '(PrintNym (if (> x 5) 100 200)) global-env)
-
-;; Definindo função dobro
-(interpret '(nymconst dobro (nymlambda (x) (* x 2))) global-env)
-
-;; Usando função dobro (20)
-(interpret '(PrintNym (dobro 10)) global-env)
+(displayln (string-append "Resultado da soma: " (number->string resultado)))
--- src/syntax/Variables.rkt
+++ src/syntax/Variables.rkt
@@ -1,19 +1,27 @@
 #lang racket
 
-
 ;; Representa uma constante literal, tipo número ou string
 (struct nymconst (value) #:transparent)
 
 ;; Representa uma expressão let com um nome, um valor e um corpo
 (struct nymlet (name value body) #:transparent)
 
+;; Representa a alteração de valor de uma variável já definida
+(struct nymset (name new-value) #:transparent)
 
-; -------------- Testes -------------- ;
+
+;; ------------------------------------
+;; Testes
+;; ------------------------------------
+
 ; Constnym
 (define A (nymconst 12))
 (displayln (string-append "Resultado de A é: " (number->string (nymconst-value A))))
 
 ; Letnym
 (define B (nymlet 'x 10 'x))
-(displayln (string-append "Resultado de B é: " (number->string (nymlet-value B))))
+(displayln (string-append "Resultado de x é: " (number->string (nymlet-value B))))
 
+; Setnym
+(define C (nymset 'x 99))
+(displayln (string-append "Mudando valor de 'x' para: " (number->string (nymset-new-value C))))
